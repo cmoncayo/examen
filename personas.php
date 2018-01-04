e8c7546e82a836dabd88854a2b96447fbd530dc0
cmoncayo <33964816+cmoncayo@users.noreply.github.com>
2018-01-04 13:24:28 -0500

    Create personas.php
    primera edicion

---
+++ personas.php
@@ -0,0 +1,13 @@
+<?php
+class personas{
+    protected $nombre;
+public function getNombre(){
+	return $this->nombre;
+	}
+public function setNombre($nombre){
+	$this->nombre 0 $nombre;
+public function talk() {
+	return "feliz año nuevo ";
+	
+	}
+    }
